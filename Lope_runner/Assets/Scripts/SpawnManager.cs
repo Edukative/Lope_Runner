08d9bdfa93ba4f825bdf982a0ad62378b038fd22
Edukative <56083723+Edukative@users.noreply.github.com>
2019-10-24 19:33:24 +0200

    Update 1
    Diferent obstacles, random obstacles and change gravity.

--- Lope_runner/Assets/Scripts/SpawnManager.cs
+++ Lope_runner/Assets/Scripts/SpawnManager.cs
@@ -4,11 +4,12 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
-    public GameObject obstaclePrefab;
+    public GameObject[] obstacles;
     private Vector3 spawnPos = new Vector3(25, 0, 0); // the position it will spawn
+    public int obstacleIndex;
 
     private float startDelay = 2;
-    private float repeatRate = 2;
+    private float repeatRate = 0.7f;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,11 +19,15 @@ public class SpawnManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     void SpawnObstacle()
     {
-        Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation); // spawn obstacle
+        int obstacleIndex = Random.Range(0, obstacles.Length);
+        Instantiate(obstacles[obstacleIndex], spawnPos,
+            obstacles[obstacleIndex].transform.rotation);// spawn obstacle
     }
-}
+
+    
+}  
